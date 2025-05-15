5e4fe84fac79f443aa6ac5f8360aa478ae4c3ad8
Ishwor-git <raishwor2@gmail.com>
2025-05-15 14:17:24 +0000

    writing more test case

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,7 +6,6 @@ from src.sentimentAnalysis.api import app
 # import os
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
-
 client = TestClient(app)
 
 def test_predict_sentiment():
@@ -16,3 +15,18 @@ def test_predict_sentiment():
     assert "label" in data
     assert "score" in data
     assert data["label"] in ["POSITIVE", "NEGATIVE"]
+
+
+def test_positive_sentiment():
+    response = client.post("/predict", json={"text": "I absolutely love this product!"})
+    assert response.status_code == 200
+    data = response.json()
+    assert data["label"] in ["POSITIVE", "LABEL_1"]
+    assert data["score"] > 0.5
+
+def test_negative_sentiment():
+    response = client.post("/predict", json={"text": "This is the worst experience I've ever had."})
+    assert response.status_code == 200
+    data = response.json()
+    assert data["label"] in ["NEGATIVE", "LABEL_0"]
+    assert data["score"] > 0.5
